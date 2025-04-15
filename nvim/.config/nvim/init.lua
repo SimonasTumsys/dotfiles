45573f9fc87fe946730d45427e179be77cceb153
this.simas <simonas.tumsys@gmail.com>
2025-04-15 15:40:42 +0300

    added lazy-nvim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,10 +1,2 @@
-
-vim.g.mapleader = " "
-
-local keymap = vim.keymap.set
-local opts = { noremap = true, silent = true }
-
-keymap({ "n", "v" }, "<Leader>y", '"+y', opts)
-keymap({ "n", "v" }, "<Leader>p", '"+p', opts)
-keymap({ "n", "v" }, "<Leader>d", '"+d', opts)
-
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
