75bcfbb7348879e36eeaa11d23dbe24e1972484b
Simas <simonas.tumsys@gmail.com>
2026-09-15 12:26:19 +0300

    updated i3 config, fixed treesitter

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -2,7 +2,7 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function ()
-      require("nvim-treesitter.configs").setup({
+      require("nvim-treesitter.config").setup({
         ensure_installed = { "lua", "vim", "vimdoc", "toml", "python", "typescript", "html" },
         auto_install = true,
         highlight = {
